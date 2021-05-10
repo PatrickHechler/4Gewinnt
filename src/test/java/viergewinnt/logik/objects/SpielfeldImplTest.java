fa3ae7720eab9b81d27883ded10b447864570077
Patrick <Patrick@Coruscant.Speedport_W_724V_01011603_07_002>
2021-05-10 10:44:19 +0200

    more tests

--- src/test/java/viergewinnt/logik/objects/SpielfeldImplTest.java
+++ src/test/java/viergewinnt/logik/objects/SpielfeldImplTest.java
@@ -2,10 +2,7 @@ package viergewinnt.logik.objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import viergewinnt.logik.interfaces.Spieler;
 import viergewinnt.logik.interfaces.Spielfeld;
@@ -56,6 +53,40 @@ class SpielfeldImplTest {
 		sf.steinEinwerfen(s1, 0);
 		sf.steinEinwerfen(s1, 0);
 		assertThrows(RuntimeException.class, () -> sf.steinEinwerfen(s2, 0));// reihe sollte voll sein
+		sf.steinEinwerfen(s1, 1);
+		sf.steinEinwerfen(s2, 1);
+		sf.steinEinwerfen(s2, 1);
+		sf.steinEinwerfen(s1, 1);
+		sf.steinEinwerfen(s1, 1);
+		assertThrows(RuntimeException.class, () -> sf.steinEinwerfen(s2, 0));// reihe sollte voll sein
+		assertThrows(RuntimeException.class, () -> sf.steinEinwerfen(s2, 1));// reihe sollte voll sein
+		sf.steinEinwerfen(s1, 2);
+		sf.steinEinwerfen(s2, 2);
+		sf.steinEinwerfen(s2, 2);
+		sf.steinEinwerfen(s1, 2);
+		sf.steinEinwerfen(s1, 2);
+		assertThrows(RuntimeException.class, () -> sf.steinEinwerfen(s2, 0));// reihe sollte voll sein
+		assertThrows(RuntimeException.class, () -> sf.steinEinwerfen(s2, 1));// reihe sollte voll sein
+		assertThrows(RuntimeException.class, () -> sf.steinEinwerfen(s2, 2));// reihe sollte voll sein
+		sf.steinEinwerfen(s1, 3);
+		sf.steinEinwerfen(s2, 3);
+		sf.steinEinwerfen(s2, 3);
+		sf.steinEinwerfen(s1, 3);
+		sf.steinEinwerfen(s1, 3);
+		assertThrows(RuntimeException.class, () -> sf.steinEinwerfen(s2, 0));// reihe sollte voll sein
+		assertThrows(RuntimeException.class, () -> sf.steinEinwerfen(s2, 1));// reihe sollte voll sein
+		assertThrows(RuntimeException.class, () -> sf.steinEinwerfen(s2, 2));// reihe sollte voll sein
+		assertThrows(RuntimeException.class, () -> sf.steinEinwerfen(s2, 3));// reihe sollte voll sein
+		sf.steinEinwerfen(s1, 4);
+		sf.steinEinwerfen(s2, 4);
+		sf.steinEinwerfen(s2, 4);
+		sf.steinEinwerfen(s1, 4);
+		sf.steinEinwerfen(s1, 4);
+		assertThrows(RuntimeException.class, () -> sf.steinEinwerfen(s2, 0));// reihe sollte voll sein
+		assertThrows(RuntimeException.class, () -> sf.steinEinwerfen(s2, 1));// reihe sollte voll sein
+		assertThrows(RuntimeException.class, () -> sf.steinEinwerfen(s2, 2));// reihe sollte voll sein
+		assertThrows(RuntimeException.class, () -> sf.steinEinwerfen(s2, 3));// reihe sollte voll sein
+		assertThrows(RuntimeException.class, () -> sf.steinEinwerfen(s2, 4));// reihe sollte voll sein
 	}
 	
 	@Test
@@ -100,6 +131,42 @@ class SpielfeldImplTest {
 		sf.steinEinwerfen(s2, 4);
 		sf.steinEinwerfen(s2, 4);
 		sf.steinEinwerfen(s1, 4);
+	}
+	
+	@Test
+	void testSteinEinwerfenDiffrentSizes() {
+		for (int reihenAnz = 0; reihenAnz < 25; reihenAnz ++ ) {
+			for (int maxGrr = 0; maxGrr < 25; maxGrr ++ ) {
+				sf = new SpielfeldImpl(reihenAnz, maxGrr);
+				reihenTesten(maxGrr, reihenAnz);
+			}
+		}
+	}
+	
+	@Test
+	void testSteinEinwerfenDiffrentHugeSizes() {
+		for (int reihenAnz = 0; reihenAnz < 100; reihenAnz ++ ) {
+			for (int maxGrr = 0; maxGrr < 100; maxGrr ++ ) {
+				sf = new SpielfeldImpl(reihenAnz, maxGrr);
+				reihenTesten(maxGrr, reihenAnz);
+			}
+		}
+	}
+	
+	private void reihenTesten(int magGr, int reihenAnz) {
+		assertThrows(IndexOutOfBoundsException.class, () -> sf.steinEinwerfen(s1, -1));
+		for (int ii = 0; ii < reihenAnz; ii ++ ) {
+			for (int i = 0; i < magGr; i ++ ) {
+				sf.steinEinwerfen(s1, ii);
+			}
+			try {
+				sf.steinEinwerfen(s1, ii);
+				fail("no exeption!");
+			} catch (RuntimeException re) {
+				if (re.getClass() != RuntimeException.class) fail(re);
+			}
+		}
+		assertThrows(IndexOutOfBoundsException.class, () -> sf.steinEinwerfen(s1, reihenAnz));
 	}
 	
 }
